9535f2d89488e6fd225bde27f2d1d7b42bd87c02
Sayd Karam <onltrabalho@gmail.com>
2023-09-18 15:56:28 -0300

    refactor: :art: Change Globalstyles

--- src/styles/Globalstyles.js
+++ src/styles/Globalstyles.js
@@ -1,3 +1,21 @@
-export default function Globalstyles() {
-  return <div></div>
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 16px;
 }
+
+body{
+    font-family: 'Sora', sans-serif;
+    background-color: #f6f5fc;
+}
+button {
+    cursor: pointer;
+}
+`;
+
+export default GlobalStyles;
