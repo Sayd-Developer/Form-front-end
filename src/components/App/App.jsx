432518aea1acbd0bc36c032c372d4bb5ed5124c1
Sayd Karam <onltrabalho@gmail.com>
2023-09-18 15:56:04 -0300

    refactor: :art: Change App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import GlobalStyles from "../../styles/GlobalStyles";
+import Home from "../Home/index";
+
+export default function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <Home />
+        </>
+    );
+}
